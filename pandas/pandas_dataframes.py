20bbd3e2a0d607bdcd2f97b693eea0d9394e700c
aburk3 <aburk3@gmail.com>
2020-04-16 06:57:40 -0500

    Added code for manipulating/accessing columns using pandas series and data-frames.

--- pandas/pandas_dataframes.py
+++ pandas/pandas_dataframes.py
@@ -10,4 +10,24 @@ index = ['A', 'B', 'C', 'D', 'E']
 # Set seed to get SAME random numbers
 np.random.seed(42)
 data = randint(-100, 100, (5, 4))
-print(data)
+# print(data)
+
+df = pd.DataFrame(data, index, columns)
+# print(df)
+
+# print(df['W'])
+
+# In a dataframe, the columns are just a panda series
+# print(type(df['W']))
+# => type: pandas.core.series.Series
+# print(type(df))
+# => type: pandas.core.frame.DataFrame
+
+# Double-bracket notation for multiple coulmns
+# print(df[['W', 'Z']])
+
+# Create new column from pre-existing data-frame
+df['new'] = df['W'] + df['Y']
+
+# Delete a column - not permanent
+# print(df.drop('new', axis=1))
